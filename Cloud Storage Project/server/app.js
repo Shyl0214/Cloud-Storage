3a702cdd7b1ccb001ed62d28d3d7fdaf74a0c768
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-01 14:21:40 +0530

    Added open and download functionality

--- Cloud Storage Project/server/app.js
+++ Cloud Storage Project/server/app.js
@@ -1,1 +1,34 @@
-console.log("Hello from server");
+import express from "express";
+import { readdir } from "fs/promises";
+
+const app = express();
+
+app.disable("x-powered-by");
+app.use(express.json());
+
+// Enabling Cors 
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+// this will server the static files
+const serveStatic = express.static("storage");
+
+// serving files ( custom middleware )
+app.use((req, res, next) => {
+  if (req.query.action === "download") {
+    res.set("Content-Disposition", "attachment");
+  }
+  serveStatic(req, res, next);
+});
+
+// serving dir content 
+app.get("/", async (req, res) => {
+  const filesList = await readdir("./storage");
+  res.json(filesList);
+});
+
+app.listen(4000, (req, res) => {
+  console.log("Server started");
+});
