859bcf0bc1ccd245080dd806429a58704ab14901
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-01 15:44:29 +0530

    Added delete functionlity

--- Cloud Storage Project/server/app.js
+++ Cloud Storage Project/server/app.js
@@ -1,32 +1,54 @@
+import { error } from "console";
 import express from "express";
-import { readdir } from "fs/promises";
+import { readdir, rm } from "fs/promises";
 
 const app = express();
 
 app.disable("x-powered-by");
+
 app.use(express.json());
 
-// Enabling Cors 
+// Enabling Cors
 app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+  });
   next();
 });
 
 // this will server the static files
 const serveStatic = express.static("storage");
 
-// serving files ( custom middleware )
-app.use((req, res, next) => {
-  if (req.query.action === "download") {
-    res.set("Content-Disposition", "attachment");
-  }
-  serveStatic(req, res, next);
-});
-
-// serving dir content 
+// serving dir content
 app.get("/", async (req, res) => {
   const filesList = await readdir("./storage");
   res.json(filesList);
+});
+
+// dynamic routing
+app.get("/:fileName", (req, res) => {
+  console.log(req.query);
+  if (req.query.action === "download") {
+    res.set("Content-Disposition", "attachment");
+  }
+  const { fileName } = req.params;
+  console.log({ fileName });
+  res.sendFile(`${import.meta.dirname}/storage/${fileName}`);
+});
+
+// delete
+app.delete("/:fileName", async (req, res) => {
+  const { fileName } = req.params;
+  const filePath = `./storage/${fileName}`;
+  console.log(filePath);
+  try {
+    // throw new error();
+    await rm(filePath);
+    res.json({ message: "File delete successfully" });
+  } catch (err) {
+    res.status(404).json({ message: "File not found" });
+  }
 });
 
 app.listen(4000, (req, res) => {
