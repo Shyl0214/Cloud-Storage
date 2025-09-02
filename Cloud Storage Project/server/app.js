89002ed72b31f406d291632e5f239f515c66c520
Shailesh Kumar singh <Shyl0214@users.noreply.github.com>
2025-09-02 16:29:55 +0530

    implemented upload functionality in backend

--- Cloud Storage Project/server/app.js
+++ Cloud Storage Project/server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { createWriteStream } from "fs";
 import { readdir, rm, rename } from "fs/promises";
 
 const app = express();
@@ -27,7 +28,7 @@ app.get("/", async (req, res) => {
 });
 
 // dynamic routing
-// get 
+// get
 app.get("/:fileName", (req, res) => {
   console.log(req.query);
   if (req.query.action === "download") {
@@ -36,6 +37,30 @@ app.get("/:fileName", (req, res) => {
   const { fileName } = req.params;
   console.log({ fileName });
   res.sendFile(`${import.meta.dirname}/storage/${fileName}`);
+});
+
+// upload
+app.post("/:fileName", (req, res) => {
+  console.log(req.params.fileName);
+  const { fileName } = req.params;
+  const writeStream = createWriteStream(`./storage/${req.params.fileName}`);
+  req.pipe(writeStream);
+  req.on("end", () => {
+    res.json({ message: "File uploaded successfully" });
+  });
+});
+
+//update
+app.patch("/:fileName", async (req, res) => {
+  const { fileName } = req.params;
+  console.log(fileName);
+  console.log(req.body.newFilename);
+  try {
+    await rename(`./storage/${fileName}`, `./storage/${req.body.newFilename}`);
+    res.json({ message: "Renamed successfully" });
+  } catch (err) {
+    res.status(404).json({ message: "Error renaming file" });
+  }
 });
 
 // delete
@@ -49,20 +74,6 @@ app.delete("/:fileName", async (req, res) => {
     res.json({ message: "File delete successfully" });
   } catch (err) {
     res.status(404).json({ message: "File not found" });
-  }
-});
-
-//update 
-app.patch("/:fileName", async (req, res) => {
-  const { fileName } = req.params;
-  console.log(fileName);
-  console.log(req.body.newFilename);
-
-  try {
-    await rename(`./storage/${fileName}`, `./storage/${req.body.newFilename}`);
-    res.json({ message: "Renamed successfully" });
-  } catch (err) {
-    res.status(404).json({ message: "Error renaming file" });
   }
 });
 
